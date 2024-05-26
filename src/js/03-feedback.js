3c3aa8af0178dfa0fe1560efdca0f054ac039a91
Volodymyr Vovnenko <vovnenko.volodymyr@gmail.com>
2024-05-26 12:49:09 +0300

    implement task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,33 @@
+import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = 'feedback-form-state';
+const form = document.querySelector('.feedback-form');
+const formElements = [form.elements['email'], form.elements['message']];
+
+const extractFormData = () =>
+  formElements.reduce((acc, element) => {
+    return { ...acc, [element.name]: element.value.trim() };
+  }, {});
+
+const applyFormData = formData =>
+  formElements.forEach(
+    element => (element.value = formData[element.name] || '')
+  );
+
+function onInputData() {
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(extractFormData()));
+}
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  console.log(extractFormData());
+
+  localStorage.removeItem(STORAGE_KEY);
+  event.currentTarget.reset();
+}
+
+applyFormData(JSON.parse(localStorage.getItem(STORAGE_KEY)) || {});
+
+form.addEventListener('input', throttle(onInputData, 500));
+form.addEventListener('submit', onFormSubmit);
