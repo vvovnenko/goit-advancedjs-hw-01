1e304f3d18d72bd04ea8ed570e449606b38ca71f
Volodymyr Vovnenko <vovnenko.volodymyr@gmail.com>
2024-05-26 11:32:34 +0300

    implement task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = 'videoplayer-current-time';
+
+const onTimeupdate = function ({ seconds }) {
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(seconds));
+};
+
+const getSeconds = () => JSON.parse(localStorage.getItem(STORAGE_KEY)) || 0;
+
+const iframe = document.querySelector('iframe#vimeo-player');
+const player = new Player(iframe);
+
+player.setCurrentTime(getSeconds());
+
+player.on('timeupdate', throttle(onTimeupdate, 1000));
